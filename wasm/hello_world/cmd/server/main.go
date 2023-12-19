6d1b4ab7f12b8f46e457c933fb1207a5215346c4
Mukesh Kumar Chaudhary <mukezhz@gmail.com>
2023-12-19 23:02:07 +0545

    feat: add basic hello world from wasm golang

---
+++ wasm/hello_world/cmd/server/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	err := http.ListenAndServe(":9090", http.FileServer(http.Dir("assets")))
+	if err != nil {
+		fmt.Println("Failed to start server", err)
+		return
+	}
+	fmt.Println("RUNNING ON PORT 9090")
+}
---
+++ wasm/hello_world/go.mod
@@ -0,0 +1,3 @@
+module github.com/mukezhz/learn/wasm/hello_world
+
+go 1.21.5
