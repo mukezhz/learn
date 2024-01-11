a5de6e6b8684cf9b35f37710929557a684330b91
Mukesh Kumar Chaudhary <mukezhz@gmail.com>
2024-01-11 08:22:04 +0545

    refactor: changed test which aligns to gherkin language

--- golang/buy_book_bdd/buy_book_bdd_test.go
+++ golang/buy_book_bdd/buy_book_bdd_test.go
@@ -6,67 +6,69 @@ import (
 	. "github.com/onsi/gomega"
 )
 
-var _ = Describe("Scenario: Buy Book From Shop", func() {
-	Context("Given: no book exists", func() {
-		var (
-			bookRepository book.BookRepository
-			bookService    book.BookService
-		)
-		BeforeEach(func() {
-			bookRepository = book.NewBookRepository()
-			bookService = book.NewBookService(bookRepository)
-		})
-		When("A user try to list a books", func() {
-			It("Then: Should have empty books", func() {
-				books := bookService.ListBooks()
-				Ω(books).Should(BeEmpty())
+var _ = Describe("Feature: CRUD of Book", func() {
+	Describe("Scenario: User try to list, get and add a book.", func() {
+		Context("Given: No book exists", func() {
+			var (
+				bookRepository book.BookRepository
+				bookService    book.BookService
+			)
+			BeforeEach(func() {
+				bookRepository = book.NewBookRepository()
+				bookService = book.NewBookService(bookRepository)
+			})
+			When("A user try to list a books", func() {
+				It("Then: Should have empty books", func() {
+					books := bookService.ListBooks()
+					Ω(books).Should(BeEmpty())
+				})
+			})
+			When("A user try to get a books", func() {
+				bookName := "ikigai"
+				book, err := bookService.GetBook(bookName)
+				It("Then: Should have an error", func() {
+					Ω(err).Should(HaveOccurred())
+				})
+				It("Then: Should have an empty data", func() {
+					Ω(book.Name).Should(BeEmpty())
+				})
+			})
+			When("A user try to add a book", func() {
+				It("Then: Should have one book And Error Should Not have occured", func() {
+					book, err := bookService.AddBook(book.NewBook("Power of Now", "Now"))
+					books := bookService.ListBooks()
+					Ω(book.Name).ShouldNot(BeEmpty())
+					Ω(len(books)).Should(Equal(1))
+					Ω(err).ShouldNot(HaveOccurred())
+				})
 			})
 		})
-		When("A user try to get a books", func() {
-			bookName := "ikigai"
-			book, err := bookService.GetBook(bookName)
-			It("Then: Should have an error", func() {
-				Ω(err).Should(HaveOccurred())
-			})
-			It("Then: Should have an empty data", func() {
-				Ω(book.Name).Should(BeEmpty())
-			})
-		})
-		When("A user try to add a book", func() {
-			It("Then: Should have one book And Error Should Not have occured", func() {
-				book, err := bookService.AddBook(book.NewBook("Power of Now", "Now"))
-				books := bookService.ListBooks()
-				Ω(book.Name).ShouldNot(BeEmpty())
-				Ω(len(books)).Should(Equal(1))
-				Ω(err).ShouldNot(HaveOccurred())
-			})
-		})
-	})
 
-	Context("Given: 1 book exists", func() {
-		var (
-			bookRepository book.BookRepository
-			bookService    book.BookService
-			bookName       string
-			newBook        book.Book
-		)
-		BeforeEach(func() {
-			bookRepository = book.NewBookRepository()
-			bookService = book.NewBookService(bookRepository)
-			bookName = "ikigai"
-			newBook, _ = bookService.AddBook(book.NewBook(bookName, "IDK"))
-		})
-		When("A user try to list a books", func() {
-			It("Then: Should Not have empty books", func() {
-				books := bookService.ListBooks()
-				Ω(books).ShouldNot(BeEmpty())
+		Context("Given: 1 book exists", func() {
+			var (
+				bookRepository book.BookRepository
+				bookService    book.BookService
+				bookName       string
+				newBook        book.Book
+			)
+			BeforeEach(func() {
+				bookRepository = book.NewBookRepository()
+				bookService = book.NewBookService(bookRepository)
+				bookName = "ikigai"
+				newBook, _ = bookService.AddBook(book.NewBook(bookName, "IDK"))
 			})
-		})
-		When("A user try to get a book", func() {
-			It("Then: Should Not have an error And Should have an book with provided book name", func() {
-				b, err := bookService.GetBook(bookName)
-				Ω(err).ShouldNot(HaveOccurred())
-				Ω(b.Name).Should(Equal(newBook.Name))
+			When("A user try to list a books", func() {
+				It("Then: Should Not have empty books", func() {
+					books := bookService.ListBooks()
+					Ω(books).ShouldNot(BeEmpty())
+				})
+			})
+			When("A user try to get a book", func() {
+				It("Then: Should Not have an error And Should have an book with provided book name", func() {
+					b, err := bookService.GetBook(bookName)
+					Ω(err).ShouldNot(HaveOccurred())
+					Ω(b.Name).Should(Equal(newBook.Name))
+				})
 			})
 		})
 	})
