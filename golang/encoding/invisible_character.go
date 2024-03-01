2242892476f929eb643f67f24f304ba2362e4967
Mukesh Kumar Chaudhary <mukezhz@gmail.com>
2024-03-01 10:46:35 +0545

    chore: print str

--- golang/encoding/invisible_character.go
+++ golang/encoding/invisible_character.go
@@ -8,6 +8,8 @@ import (
 func main() {
 	str := "‪0312345678"
 
+	fmt.Println(str)
+
 	for i, rune := range str {
 		if !unicode.IsPrint(rune) {
 			fmt.Printf("Non-printable character at position %d: %U\n", i, rune)
