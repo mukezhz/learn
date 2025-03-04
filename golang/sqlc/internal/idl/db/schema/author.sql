6b3b069b9b430a7ae514f2bf8eb6226afaf3d51d
Mukesh Kumar Chaudhary <mukezhz@gmail.com>
2025-03-04 16:20:10 +0545

    feat: update authors table schema and add books table with migrations

--- golang/sqlc/internal/idl/db/schema/author.sql
+++ golang/sqlc/internal/idl/db/schema/author.sql
@@ -1,8 +1,8 @@
 CREATE TABLE authors (
   id BIGSERIAL PRIMARY KEY,
   name text NOT NULL,
-  bio text
+  bio text,
+  age VARCHAR(10),
+  created_at TIMESTAMPTZ DEFAULT NOW(),
+  updated_at TIMESTAMPTZ DEFAULT NOW()
 );
-
-ALTER TABLE authors ADD COLUMN created_at TIMESTAMPTZ DEFAULT NOW();
-ALTER TABLE authors ADD COLUMN updated_at TIMESTAMPTZ DEFAULT NOW();
---
+++ golang/sqlc/migrations/20250304103453.sql
@@ -0,0 +1,2 @@
+-- Modify "authors" table
+ALTER TABLE "public"."authors" ADD COLUMN "age" character varying(10) NULL;
